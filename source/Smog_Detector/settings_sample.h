795ca1468843f843c17c91024a571b42a742c6a6
Lukasz Szczurowski <szczurowski.lukasz@gmail.com>
2019-11-03 12:58:19 +0100

    Added OLED_MODE and MINIMAL_PMS_WAIT_TIME to settings file

--- source/Smog_Detector/settings_sample.h
+++ source/Smog_Detector/settings_sample.h
@@ -1,33 +1,39 @@
 // -- Setup debug mode - Serial messages will be printed
-#define DEBUG_MODE          0
+#define DEBUG_MODE
+
+// -- Turn On/Off Oled display
+// #define OLED_MODE
+
+// -- Minimal time to wait for dust sensor read
+#define MINIMAL_PMS_WAIT_TIME   2500
 
 // -- Setup your own Wifi settings  ---------------
-#define STA_SSID            "YourSSID"              // [Ssid] Wifi SSID
+#define STA_SSID                "YourSSID"              // [Ssid] Wifi SSID
 
-#define STA_PASS            "YourWifiPassword"      // [Password] Wifi password
+#define STA_PASS                "YourWifiPassword"      // [Password] Wifi password
 
-#define STA_HOST_NAME       "YourHostName"          // [HostName] Wifi Host name
+#define STA_HOST_NAME           "YourHostName"          // [HostName] Wifi Host name
 
-// -- Setup your own MQTT settings  ---------------
-#define MQTT_HOST           "your-mqtt-server.com"  // [MqttHost]
+// -- Setup your own MQTT settings  --------------------
+#define MQTT_HOST               "your-mqtt-server.com"  // [MqttHost]
 
-#define MQTT_PORT           1883                    // [MqttPort] MQTT port (10123 on CloudMQTT)
+#define MQTT_PORT               1883                    // [MqttPort] MQTT port (10123 on CloudMQTT)
 
-#define MQTT_USER           "YourMqttUser"          // [MqttUser] Optional user
+#define MQTT_USER               "YourMqttUser"          // [MqttUser] Optional user
 
-#define MQTT_PASS           "YourMqttPass"          // [MqttPassword] Optional password
+#define MQTT_PASS               "YourMqttPass"          // [MqttPassword] Optional password
 
 // -- Master parameter control --------------------
-#define PROJECT             "smog"                  // PROJECT is used as the default topic delimiter
+#define PROJECT                 "smog"                  // PROJECT is used as the default topic delimiter
 
 // -- MQTT topics ---------------------------------
 // Example "tele/smog/bedroom/" up to 80 characers
-#define SUB_FULLTOPIC       "cmnd/smog/"            // [FullTopic] Subscribe full topic name
-#define PUB_FULLTOPIC       "stat/smog/"            // [FullTopic] Publish full topic name
-#define TELE_FULLTOPIC      "tele/smog/"            // [FullTopic] Telemetry and Publish full topic name
+#define SUB_FULLTOPIC           "cmnd/smog/"            // [FullTopic] Subscribe full topic name
+#define PUB_FULLTOPIC           "stat/smog/"            // [FullTopic] Publish full topic name
+#define TELE_FULLTOPIC          "tele/smog/"            // [FullTopic] Telemetry and Publish full topic name
 
 // -- MQTT - Publish ------------------------------
-#define STAT_PERIOD            300                  // [StatPeriod] Publish interval (0 = disable, 10 - 3600 seconds)
+#define STAT_PERIOD             300                  // [StatPeriod] Publish interval (0 = disable, 10 - 3600 seconds)
 
 // -- MQTT - Telemetry ----------------------------
-#define TELE_ON_POWER          0                    // [SetOption59] Send information about connection on power
+#define TELE_ON_POWER           0                    // [SetOption59] Send information about connection on power
